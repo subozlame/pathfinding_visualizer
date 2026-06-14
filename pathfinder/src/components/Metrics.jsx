7471b77c5ffeafdad747cf5496914c4edb46a775
subozlame <213662061+subozlame@users.noreply.github.com>
2026-06-15 00:36:52 +0545

    metrics comp

--- pathfinder/src/components/Metrics.jsx
+++ pathfinder/src/components/Metrics.jsx
@@ -1,9 +1,32 @@
 function Metrics({
-    visitedCount,
-    pathLength,
-    time,
-}){
-    return(
-        <div></div>
-    )
+  visitedCount,
+  pathLength,
+  time,
+}) {
+  return (
+    <div className="flex gap-6 justify-center mt-4 text-white">
+      <div>
+        Visited:{" "}
+        <span className="text-blue-400">
+          {visitedCount}
+        </span>
+      </div>
+
+      <div>
+        Path:{" "}
+        <span className="text-yellow-400">
+          {pathLength}
+        </span>
+      </div>
+
+      <div>
+        Time:{" "}
+        <span className="text-green-400">
+          {time} ms
+        </span>
+      </div>
+    </div>
+  );
 }
+
+export default Metrics;
